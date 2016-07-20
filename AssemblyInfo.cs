acdd903abb0073500233101f0f3b11b064418f25
Michael Ketting <michael.ketting@rubicon.eu>
2016-07-20 14:02:20 +0200

    Add assembly metadata and nuspec file to publish Microsoft.Cci as a NuGet package with support for .NET 4.5

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle ("Microsoft.Cci (forked to support .NET 4.5)")]
+[assembly: AssemblyDescription (@"Provides a fork of the Microsoft.Cci library, compatible with .NET 4.5.
+GitHub SHA: https://github.com/MichaelKetting/Microsoft.Cci/tree/126992bbf155dc8c6ba30cb0e9a12acc507c2579")]
+[assembly: AssemblyCompany ("Microsoft Corporation")]
+[assembly: AssemblyProduct ("Microsoft CCI")]
+[assembly: AssemblyCopyright ("(c) Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyVersion ("4.0.0.0")]
+[assembly: AssemblyFileVersion ("4.0.0.0")]
+[assembly: AssemblyInformationalVersion ("4.0.0-rc4+126992bbf155dc8c6ba30cb0e9a12acc507c2579")]
