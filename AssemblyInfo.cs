6e6866cfad73a0e7baaf85506ff27a49e4eb86e0
Michael Ketting <michael.ketting@rubicon.eu>
2016-07-24 19:20:46 +0200

    Update AssemblyInfo for v4.0.0.1

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 
 [assembly: AssemblyTitle ("Microsoft.Cci (forked to support .NET 4.5)")]
 [assembly: AssemblyDescription (@"Provides a fork of the Microsoft.Cci library, compatible with .NET 4.5.
-GitHub SHA: https://github.com/MichaelKetting/Microsoft.Cci/tree/126992bbf155dc8c6ba30cb0e9a12acc507c2579")]
+GitHub SHA: https://github.com/MichaelKetting/Microsoft.Cci/tree/a1354b339399724bf64e276481b057dc002cad41")]
 [assembly: AssemblyCompany ("Microsoft Corporation")]
 [assembly: AssemblyProduct ("Microsoft CCI")]
 [assembly: AssemblyCopyright ("(c) Microsoft Corporation.  All rights reserved.")]
 [assembly: AssemblyVersion ("4.0.0.0")]
-[assembly: AssemblyFileVersion ("4.0.0.0")]
-[assembly: AssemblyInformationalVersion ("4.0.0-rc4+126992bbf155dc8c6ba30cb0e9a12acc507c2579")]
+[assembly: AssemblyFileVersion ("4.0.0.1")]
+[assembly: AssemblyInformationalVersion ("4.0.0-rc4+a1354b339399724bf64e276481b057dc002cad41")]
